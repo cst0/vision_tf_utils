d44fd6d63f4b9da6863005ab2b5480475ca1b3b5
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-03-22 11:14:02 -0400

    reformat for pep8

--- src/visible_frame_retriever.py
+++ src/visible_frame_retriever.py
@@ -10,41 +10,50 @@ from image_geometry.cameramodels import PinholeCameraModel
 from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
 from sensor_msgs.msg import Image, CameraInfo
 
+
 class PinholeCameraModelWithFov(PinholeCameraModel):
     """
     Extension of pinhole camera model to allow calculation of the field of view.
     Should be covered by https://github.com/ros-perception/vision_opencv/pull/428
     but this is not yet merged.
     """
+
     def fovX(self):
         K = self.fullIntrinsicMatrix()
         assert K is not None
-        alphax = K[0,0]
+        alphax = K[0, 0]
         return 2 * atan(self.width / (2 * alphax))
+
     def fovY(self):
         K = self.fullIntrinsicMatrix()
         assert K is not None
-        alphay = K[1,1]
+        alphay = K[1, 1]
         return 2 * atan(self.height / (2 * alphay))
+
 
 class VisibleFrameRetriever:
     """
     Retrieves visible frames, and makes the output available as a service.
     """
+
     def __init__(self):
         self.tf_buff = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buff)
 
-        self.srv = rospy.Service('evaluate', Trigger, self.run)
+        self.srv = rospy.Service("evaluate", Trigger, self.run)
 
-        self.sub_camerainfo = rospy.Subscriber('/camera/color/camera_info', CameraInfo, self.cb_camerainfo)
-        self.sub_image      = rospy.Subscriber('/camera/color/image_raw',   Image,      self.cb_image)
+        self.sub_camerainfo = rospy.Subscriber(
+            "/camera/color/camera_info", CameraInfo, self.cb_camerainfo
+        )
+        self.sub_image = rospy.Subscriber(
+            "/camera/color/image_raw", Image, self.cb_image
+        )
 
         self.image = Image()
         self.cameramodel = PinholeCameraModelWithFov()
         self.transforms_from_cameraframe = {}
 
-    def cb_camerainfo(self, msg:CameraInfo):
+    def cb_camerainfo(self, msg: CameraInfo):
         self.cameramodel.fromCameraInfo(msg)
 
     def cb_image(self, msg):
@@ -53,14 +62,16 @@ class VisibleFrameRetriever:
     def get_tf_links(self):
         return [*(yaml.full_load(self.tf_buff.all_frames_as_yaml()).keys())]
 
-    def run(self, trig:TriggerRequest):
+    def run(self, trig: TriggerRequest):
         del trig
         links = self.get_tf_links()
         cf = self.cameramodel.tfFrame()
         self.transforms_from_cameraframe = {}
 
         for link in links:
-            self.transforms_from_cameraframe[link] = self.tf_buff.lookup_transform(cf, link, rospy.Time())
+            self.transforms_from_cameraframe[link] = self.tf_buff.lookup_transform(
+                cf, link, rospy.Time()
+            )
         horizontal_angles = {}
         vertical_angles = {}
         in_frame = []
@@ -77,26 +88,30 @@ class VisibleFrameRetriever:
             # and so |xy|cos(theta) -> theta = acos(x / sqrt(x^2+y^2)).
             # also adding a safety check for 0-degree transforms, which are very common
             # in static tf links.
-            horizontal_angles[link] = acos(x / sqrt(x*x + y*y)) if (x*x + y*y) != 0 else 0
+            horizontal_angles[link] = (
+                acos(x / sqrt(x * x + y * y)) if (x * x + y * y) != 0 else 0
+            )
             # same deal for vertical
-            vertical_angles[link] = acos(x / sqrt(x*x + z*z)) if (x*x + z*z) != 0 else 0
+            vertical_angles[link] = (
+                acos(x / sqrt(x * x + z * z)) if (x * x + z * z) != 0 else 0
+            )
 
             # getting fov. Dividing by 2 because previous operation gets angle from center,
             # but this is giving total angle.
-            fovx = (self.cameramodel.fovX())/2
-            fovy = (self.cameramodel.fovY())/2
+            fovx = (self.cameramodel.fovX()) / 2
+            fovy = (self.cameramodel.fovY()) / 2
 
             if horizontal_angles[link] < fovx and vertical_angles[link] < fovy:
                 in_frame.append(link)
 
-        return TriggerResponse(success=True,
-                message=str(in_frame))
+        return TriggerResponse(success=True, message=str(in_frame))
 
 
 def main():
-    rospy.init_node('visible_frame_retriever')
+    rospy.init_node("visible_frame_retriever")
     vfr = VisibleFrameRetriever()
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
